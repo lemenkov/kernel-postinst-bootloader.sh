a33e6d044a1348ca82f002813b445bf1e4a29c4f
Peter Lemenkov <lemenkov@gmail.com>
2013-05-15 10:39:38 +0400

    Added missing title to prerm script
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- prerm.d/kernel-prerm-bootloader.sh
+++ prerm.d/kernel-prerm-bootloader.sh
@@ -29,6 +29,11 @@ if [ -f /etc/machine-id ]; then
 	machine_id="$(cat /etc/machine-id)"
 fi
 
+title="Default OS ($version)"
+if [ -f /etc/os-release ]; then
+	title="$(grep -o -P "(?<=PRETTY_NAME=\")(.*)(?=\")" /etc/os-release) ($version)"
+fi
+
 os_id="$(echo "$title" | tr 'A-Z' 'a-z' | cut -d' ' -f1)"
 if [ -z "$os_id" ]; then
 	os_id=linux
